b50a31ef3f4c9fdb54e5ef8118503f98bbe068dc
heyitschun <heyitschun@protonmail.com>
2021-02-14 19:36:20 +0100

    upd

--- .config/scripts/get-ip.sh
+++ .config/scripts/get-ip.sh
@@ -5,8 +5,12 @@ PUBLIC_IP=`wget http://ipecho.net/plain -O - -q ; echo`
 if [ $PUBLIC_IP = "163.158.199.129" ]
 then
     echo " $PUBLIC_IP"
+    echo
+    echo \#00ff00
 else
     echo " $PUBLIC_IP"
+    echo
+    echo \#ff0000
 fi
 
 
